3d7d5feb9adc5903f3f89cb43247f5f8de9613bd
高铁峰 <1096392101@qq.com>
2019-08-14 11:43:03 +0800

    update

--- src/components/WelCome.vue
+++ src/components/WelCome.vue
@@ -26,6 +26,12 @@
 			<van-button square type="primary">方形按钮</van-button>
 			<van-button round type="danger">圆形按钮</van-button>
 		</div>
+		<div>
+			<van-button type="primary" size="large">大号按钮</van-button>
+			<van-button type="primary" size="normal">普通按钮</van-button>
+			<van-button type="primary" size="small">小型按钮</van-button>
+			<van-button type="primary" size="mini">迷你按钮</van-button>
+		</div>
 	</div>
 </template>
 
