0e55f84facc25d34699607112acd20340f411872
高铁峰 <1096392101@qq.com>
2019-08-27 09:43:13 +0800

    add js-cookie

--- src/common/js/common.js
+++ src/common/js/common.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import Qs from 'qs';
 import * as errors from '@/common/js/config';
+import {getToken} from '@/common/js/cache';
+import routes from '@/router'
 
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_WEB_API, // api的base_url//process.env.BASE_API
-  timeout: 5000 // 请求超时时间
+  timeout: 50000 // 请求超时时间
 })
 
 service.interceptors.request.use(config => {
@@ -13,16 +15,14 @@ service.interceptors.request.use(config => {
     config.data = Qs.stringify(config.data)
   }
   // Do something before request is sent
-  // let token = getToken()
-
-  // if (token) {
-  //   config.headers['TOKEN'] = token
-	// 	config.headers['Auth-Token'] = token
-  // }
+  let token = getToken()
+   if (token) {
+	 	config.headers['Auth-Token'] = token
+   }
 
    config.headers['Content-Type'] = 'application/json'
    config.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8'
-  // config.headers['Content-Type'] = 'multipart/form-data'
+   config.headers['Content-Type'] = 'multipart/form-data'
   return config
 }, error => {
   // Do something with request error
@@ -33,14 +33,19 @@ service.interceptors.request.use(config => {
 
 
 service.interceptors.response.use(response =>{
-  alert(12)
   let res = response.data
+  if (res.code == errors.ERROR_TOKEN_ILLEGAL || res.code == errors.ERROR_TOKEN) {
+      alert('需要登录')
+      routes.push({name: 'userLogin'})
+      return res;
+  }
   if(res.code != 0)
   {
     alert(res.message)
   }
   return res
 },function (error){
+  console.log(error)
   return Promise.reject(error)
 }
 )
--- src/common/js/config.js
+++ src/common/js/config.js
@@ -2,3 +2,5 @@ export const ERR_OK = 0
 
 // 未登录
 export const ERROR_TOKEN_ILLEGAL = 700
+
+export const ERROR_TOKEN = 1000
