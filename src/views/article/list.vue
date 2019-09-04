6eb0b8e8cd6f8ff2ed256b8540d3b095186f2763
tiefeng <1096392101@qq.com>
2019-09-04 21:40:33 +0800

    update

--- src/views/article/list.vue
+++ src/views/article/list.vue
@@ -1,6 +1,27 @@
 <template>
 	<div>
 		<Top />
+
+		<div>
+			<div>
+				<p class="user">
+				<van-image
+					round
+					width="4rem"
+					height="4rem"
+					src="https://img.yzcdn.cn/vant/cat.jpeg"
+					
+				/>
+				<span class="name">六年</span>
+				</p>
+			</div>
+			<div>12344eshasdjfadls lasopawopweopaewourwuawaiopwjraiarew</div>
+			<div>
+				<span>浏览数：1233</span>
+				<span>点赞数：233</span>
+			</div>
+		</div>
+
 		<van-pagination 
 		  v-model="currentPage" 
 		  :total-items="125" 
@@ -33,5 +54,8 @@
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.user {
+	left: 0px;
+}
 </style>
